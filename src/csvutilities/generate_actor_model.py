7cfcaa7170065b456d0d89513567f9e387947e18
Faraday <faraday@faradays-imac.dhcp.asu.edu>
2017-09-07 16:35:26 -0700

    added actor ranking

--- src/csvutilities/generate_actor_model.py
+++ src/csvutilities/generate_actor_model.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
+RANGE_MIN = 1
+
+RANGE_MAX = 2
+
 DATASET_ROOT_PATH = '/Users/faraday/Documents/Projects/mwdb/Mwdb_phase1/resources/phase1_dataset/'
 
 
@@ -10,12 +14,7 @@ def readcsv(name):
 def generate_actor_model(actorid):
     print("Generating actor model")
     df_tags = readcsv(DATASET_ROOT_PATH + 'mltags.csv')
-    df_actor = readcsv(DATASET_ROOT_PATH + 'movie-actor.csv')
-
     df_tags['timestamp'] = pd.to_datetime(df_tags['timestamp'], format='%Y/%m/%d %H:%M:%S')
-
-    #min_tag = (df_tags['timestamp'].groupby(df_tags['movieid']).min()).to_frame()
-    #print(min_tag.columns)
     min_tag = df_tags['timestamp'].groupby(df_tags['movieid']).min()
     min_tag = min_tag.to_frame()
     min_tag = min_tag.reset_index(level=['movieid'])
@@ -26,13 +25,30 @@ def generate_actor_model(actorid):
     max_tag.columns = ['movieid', 'timestamp_max']
     min_max_tag = pd.merge(min_tag,max_tag,on='movieid')
     df_tags_with_min_max = pd.merge(df_tags,min_max_tag,on='movieid')
-    df_tags_with_min_max['between 1-2'] = df_tags_with_min_max.apply(
+    df_tags_with_min_max['ts_rank'] = df_tags_with_min_max.apply(
         lambda row: ((row['timestamp'].timestamp() - row['timestamp_min'].timestamp())
                      / (row['timestamp_max'].timestamp() - row['timestamp_min'].timestamp())
                      if row['timestamp_max'].timestamp() != row['timestamp_min'].timestamp() else 1)
-                    * (2 - 1) + 1, axis=1)
+                    * (RANGE_MAX - RANGE_MIN) + RANGE_MIN, axis=1)
 
-    merged = pd.merge(df_actor,df_tags_with_min_max, on='movieid')
+    df_actor = readcsv(DATASET_ROOT_PATH + 'movie-actor.csv')
+    min_tag = df_actor['actor_movie_rank'].groupby(df_actor['movieid']).min()
+    min_tag = min_tag.to_frame()
+    min_tag = min_tag.reset_index(level=['movieid'])
+    max_tag = df_actor['actor_movie_rank'].groupby(df_actor['movieid']).max()
+    max_tag = max_tag.to_frame()
+    max_tag = max_tag.reset_index(level=['movieid'])
+    min_tag.columns = ['movieid', 'rank_min']
+    max_tag.columns = ['movieid', 'rank_max']
+    min_max_tag = pd.merge(min_tag, max_tag, on='movieid')
+    df_actor_with_min_max = pd.merge(df_actor, min_max_tag, on='movieid')
+    df_actor_with_min_max['rank'] = df_actor_with_min_max.apply(
+        lambda row: ((row['actor_movie_rank'] - row['rank_min'])
+                     / (row['rank_max'] - row['rank_min'])
+                     if row['rank_max'] != row['rank_min'] else 1)
+                    * (RANGE_MAX - RANGE_MIN) + RANGE_MIN, axis=1)
+
+    merged = pd.merge(df_actor_with_min_max.iloc[:,[0,1,2,5]],df_tags_with_min_max.iloc[:,[1,2,6]], on='movieid')
     merged['COUNTER'] = 1
     group_data = pd.DataFrame(merged.groupby(['actorid', 'tagid'])['COUNTER'].sum())
     term_vector = group_data.pivot_table('COUNTER', ['actorid'], 'tagid')
